c6a38a6415109b22e11fb00a6940fdca968aec95
Ramiro Angarita <msr.ramiro@gmail.com>
2025-02-06 19:30:55 -0500

    url callback

--- models/lottery.js
+++ models/lottery.js
@@ -51,7 +51,7 @@ exports.checkAndUpdatePayments = async () => {
         // 2. Iterar sobre cada número comprado
         for (const number of numbers) {
             const { id_payment } = number;
-           console.log(number);
+        
             // 3. Consultar el estado del pago en Mercado Pago
             const url = `https://api.mercadopago.com/v1/payments/search?criteria=desc&id=${id_payment}`;
             const headers = {
@@ -62,10 +62,10 @@ exports.checkAndUpdatePayments = async () => {
                 // 4. Hacer la solicitud para obtener el estado del pago
                 const response = await axios.get(url, { headers });
                 const payment = response.data.results[0];
-
+                console.log(payment,"1");
                 if (payment) {
                     const { status, status_detail } = payment;
-
+                    console.log(payment);
                     // 5. Verificar si el estado no es 'approved' o 'accredited'
                     if (status !== "approved") {
                         // 6. Llamar al SP para actualizar el estado
