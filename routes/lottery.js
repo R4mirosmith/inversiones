312433f893a3a113f63b8c2ffcb763cd7de9acdd
Ramiro Angarita <msr.ramiro@gmail.com>
2025-03-03 16:33:04 -0500

    resend

--- routes/lottery.js
+++ routes/lottery.js
@@ -224,8 +224,8 @@ router.post('/webhook', async (req, res) => {
 
                 const { data, error } = await resend.emails.send({
                   from: 'Acme <onboarding@resend.dev>',
-                  to: ['delivered@resend.dev'],
-                  subject: 'Hello World',
+                  to: ['msr.ramiro@gmail.com'],
+                  subject: 'Hello World smith',
                   html: '<strong>It works!</strong>',
                 });
 
