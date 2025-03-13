6e26a4b7defc387f50d236c3cf3703e92bf77100
Ramiro Angarita <msr.ramiro@gmail.com>
2025-03-13 10:54:59 -0500

    body webhook

--- models/lottery.js
+++ models/lottery.js
@@ -1,6 +1,6 @@
 var db = require('../db/db.js');
 exports.create = async (body) => {
-    let values=[body.identification,body.nombre,body.telefono,body.estadopayment,body.idpayment,body.cantidad,body.email];
+    let values=[body.identification,body.nombre,body.telefono,body.status,body.idpayment,body.cantidad,body.email];
     console.log(values,"pagosss")
     const promisePool = db.get().promise();
     return promisePool.query('CALL spInsertarClienteYNumeros(?,?,?,?,?,?,?)', values);
