c848366a1b2c56080951d04871c0afbf7bf5baa5
Ramiro Angarita <msr.ramiro@gmail.com>
2025-03-12 23:02:45 -0500

    update payment

--- routes/lottery.js
+++ routes/lottery.js
@@ -110,7 +110,7 @@ router.post('/pago', async (req, res) => {
 
       const { nombre, identification, telefono, email, banco_id, cantidad } = req.body;
 
-      console.log(req.body, "***");
+      // console.log(req.body, "***");
 
       // Verifica si los datos necesarios están presentes
       if (telefono && nombre && identification && email && banco_id && cantidad > 0) {
@@ -125,7 +125,7 @@ router.post('/pago', async (req, res) => {
               notification_url: "https://appmagdalena.net/apinversion/inversiones/webhook", // Reemplazar con tu URL de webhook real
               payer: {
                 entity_type: "individual",
-                first_name: nombre,
+                first_name: name_capitalize,
                 email: email,
                 identification: {
                     type: "CC",
@@ -206,7 +206,7 @@ router.post('/webhook', async (req, res) => {
        paymentId = req.body.resource; // Usamos el valor de 'resource' como paymentId
     }
     // console.log(paymentId, "webhook*********************");
-    // console.log(req.body, "webhook*********************");
+
     // console.log(req.body.data.id, "req.body.id*********************");
       const url = `https://api.mercadopago.com/v1/payments/search?&id=${paymentId}`;
       const headers = {
@@ -225,6 +225,7 @@ router.post('/webhook', async (req, res) => {
       // console.log("///////////////////////////////////////");
       if (payment) {
           const { status, status_detail } = payment;
+          console.log(req.body, "webhook*********************");
           // console.log(status);
           // console.log(status_detail);
           // 5. Verificar si el estado no es 'approved' o 'accredited'
@@ -281,177 +282,6 @@ router.post('/webhook', async (req, res) => {
 ////////////////////////////////////////////////////////////////////////
 //                     SEND MAIL
 ////////////////////////////////////////////////////////////////////////
-router.post('/sendmail',
-    async (req,res) => { try {
-      res.setHeader('Content-Type', 'application/json');
-      log.logger.info(`{"verb":"${req.method}", "path":"${req.baseUrl + req.path}", "body":"${JSON.stringify(req.body)}","user":""}`);
-            
-      //Handle validation errors
-      var errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).send(JSON.stringify({success:false,error:{code:201, message:"Request has invalid data",details:errors.mapped()}}, null, 3));
-      //Get matched data
-      const data = matchedData(req.body);
-      let mailOptions = {};
-      console.log(req.body,"************");
-      console.log(data);
-
-
-            let htmlCorreo =`<!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Notificación de Compra de Números del sorteo</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    color: #333;
-                    background-color: #f4f4f4;
-                    margin: 0;
-                    padding: 0;
-                }
-                .container {
-                    width: 100%;
-                    max-width: 600px;
-                    margin: 20px auto;
-                    background-color: #ffffff;
-                    padding: 20px;
-                    border-radius: 10px;
-                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                }
-                .header {
-                    text-align: center;
-                    margin-bottom: 20px;
-                }
-                .header img {
-                    max-width: 180px;
-                }
-                h2 {
-                    color: #2c3e50;
-                    font-size: 24px;
-                    margin-bottom: 10px;
-                }
-                p {
-                    font-size: 16px;
-                    line-height: 1.5;
-                    color: #555;
-                }
-                .important {
-                    background-color: #f8d7da;
-                    color: #721c24;
-                    padding: 10px;
-                    border-radius: 5px;
-                    margin-bottom: 20px;
-                }
-                ul {
-                    list-style-type: none;
-                    padding: 0;
-                    margin-bottom: 20px;
-                }
-                ul li {
-                    background-color: #ecf0f1;
-                    padding: 10px;
-                    margin-bottom: 5px;
-                    border-radius: 5px;
-                }
-                .footer {
-                    font-size: 14px;
-                    text-align: center;
-                    color: #888;
-                }
-                .footer a {
-                    color: #3498db;
-                    text-decoration: none;
-                }
-                .button {
-                    display: inline-block;
-                    background-color: #3498db;
-                    color: white;
-                    padding: 10px 20px;
-                    border-radius: 5px;
-                    text-align: center;
-                    font-weight: bold;
-                    text-decoration: none;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <!-- Cabecera con el logo -->
-                <div class="header">
-                    <img src="cid:logo" alt="!Melitico y ricolino¡">
-                </div>
-
-                <hr>
-                <h2>Notificación de Compra de Números del sorteo</h2>
-                <p>Hola ${req.body.name},</p>
-
-                <p>Te informamos que has adquirido exitosamente los siguientes números del sorteo en Meliticoyricolino:</p>
-
-                <!-- Lista de números de lotería -->
- 
-                    <!-- Lista dinámica de números -->
-                    <ul>
-                        ${req.body.numbers.map((num, index) => `<li>Número ${index + 1}: ${num}</li>`).join('')}
-                    </ul>
-
-                <!-- Mensaje importante -->
-                <div class="important">
-                    <p><strong>Importante:</strong> Si no completas el pago dentro de los siguientes <strong>15 minutos</strong>, tus números perderán su validez y serán liberados nuevamente para otros usuarios.</p>
-                </div>
-
-                <p>Te recomendamos completar el proceso de pago lo antes posible para garantizar tu participación en el sorteo. Si ya has finalizado el pago, por favor ignora este mensaje.</p>
-
-                <p>Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos.</p>
-
-                <p>¡Gracias por confiar en nosotros!</p>
-
-                <div class="footer">
-                    <p>Saludos cordiales,<br>El equipo de Meliticoyricolino</p>
-                    <p><a href="mailto:Carlosmariopastranadoria@gmail.com">Carlosmariopastranadoria@gmail.com</a> | <a href="https://meliticoyricolino.inletsoft.com/">Visítanos</a></p>
-                </div>
-            </div>
-        </body>
-        </html>
-`;
-                            
-  
-      const transporter = nodemailer.createTransport(
-        {
-          host:"smtp.gmail.com",
-          port:465,
-          secure:true,
-          auth:{
-            user: "Carlosmariopastranadoria@gmail.com", 
-            pass: "bnmk epcd qzgn hzln"
-          }
-        })
-        mailOptions = {
-          from: "Carlosmariopastranadoria@gmail.com",
-          to:req.body.email,
-          subject:"Equipo de Meliticoyricolino",
-          html:htmlCorreo,
-          attachments: []
-    }
-        
-      
-  
-    
-  
-  
-      //Send email
-      transporter.sendMail(mailOptions,async (error,info)=>{
-          if(error) return res.status(500).send(JSON.stringify({success:false,error:{code:301,message:error.message, details:null}}, null, 3));
-          if(data.inspeccion_id != undefined)   await InspeccionModel.UpdateStateNotificacion(data.inspeccion_id);
-          return res.status(200).send(JSON.stringify({success:true,data:{msg:"Email enviado gracias!!!"}}, null, 3));
-      })
-  
-  
-    }
-    catch(err){
-      log.logger.error(`{"verb":"${req.method}", "path":"${req.baseUrl + req.path}", "params":"${JSON.stringify(req.params)}", "query":"${JSON.stringify(req.query)}", "body":"${JSON.stringify(req.body)}","user":"", "error":"${err}"}`);
-      return res.status(500).send(JSON.stringify({success:false,error:{code:301,message:"Error in service or database", details:err}}, null, 3));
-}});
 
 ////////////////////////////////////////////////////////////////////////
 //                Get list of all Jornadas
