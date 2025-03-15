e085b6d7fc8bdc82284562762d03d7fdd85aa98c
Ramiro Angarita <msr.ramiro@gmail.com>
2025-03-15 08:54:53 -0500

    create

--- routes/lottery.js
+++ routes/lottery.js
@@ -321,7 +321,7 @@ router.post('/webhook', async (req, res) => {
           // 5. Verificar si el estado no es 'approved' o 'accredited'
           const [Response] = await lotteryModel.create({ identification,nombre,telefono,status,paymentId,cantidad,email});
 
-          console.log(Response[1], "Response create*********************");
+          console.log(Response[0], "Response create*********************");
           // console.log(Response[1][0].idPayment, "Response create*********************");
           if (status == "approved" && status_detail == "accredited") {
 
