4ccea73a9cbfd72a1b7ecc4586c25e3ee2db3294
Ramiro Angarita <msr.ramiro@gmail.com>
2025-05-08 11:45:33 -0500

    resend

--- routes/lottery.js
+++ routes/lottery.js
@@ -20,11 +20,7 @@ var router = express.Router();
 
 const ENVIRONMENT = config.ENVIRONMENT;
 
-import fetch from 'node-fetch';
-global.fetch = fetch;
-global.Headers = fetch.Headers;
-global.Request = fetch.Request;
-global.Response = fetch.Response;
+
 
 
 ////////////////////////////////////////////////////////////////////////
