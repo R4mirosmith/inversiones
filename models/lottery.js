02968819aeb829304690b2e70b4962c8e259d4b0
Ramiro Angarita <msr.ramiro@gmail.com>
2025-05-08 22:40:14 -0500

    guid

--- models/lottery.js
+++ models/lottery.js
@@ -1,8 +1,7 @@
 var db = require('../db/db.js');
 exports.create = async (body) => {
     let values=[body.identification,body.nombre,body.telefono,body.email,body.status,body.paymentId,body.cantidad,body.external_reference];
-    console.log(body.external_reference,"pagosss")
-    console.log(body,"pagosss")
+console.log(values,"values");
     const promisePool = db.get().promise();
     return promisePool.query('CALL spInsertarClienteYNumeros(?,?,?,?,?,?,?,?)', values);
 }
